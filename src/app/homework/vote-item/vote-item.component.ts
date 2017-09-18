66280a9e69a94abba5cb92d26930e9055dc400fb
itany <itany@itany.com>
2017-09-18 10:01:08 +0800

    ddd

--- src/app/homework/vote-item/vote-item.component.ts
+++ src/app/homework/vote-item/vote-item.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-vote-item',
   templateUrl: './vote-item.component.html',
   styleUrls: ['./vote-item.component.css']
 })
-export class VoteItemComponent implements OnInit {
+export class VoteItemComponent  {
 
-  constructor() { }
+  @Input()
+  username:string;
 
-  ngOnInit() {
+  @Output()
+  vote:EventEmitter<boolean> = new EventEmitter();
+
+  flag:boolean = false;
+
+  doVote(f)
+  {
+    this.flag = true;
+    this.vote.emit(f);
   }
-
 }
